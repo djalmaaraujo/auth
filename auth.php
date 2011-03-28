da3815b861aa898cc37da0db87a3981c6dac4ac1
Djalma Araújo <djalma.araujo@gmail.com>
2011-03-28 14:10:03 -0300

    Fixing identation

--- auth.php
+++ auth.php
@@ -1,84 +1,84 @@
 <?php
+
 /*
     Class: Auth
 
     Identifies and authenticates users using sessions.
 
     Dependencies:
-    - Model
-    - Session
-    - Security
+        - Model
+        - Session
+        - Security
 
-    Example:
-        - Inside your UsersController for an example, you can create you login method like the code bellow
-        (start code)
-        public function login() {
-            if ($data = $this->data) {
-                if ($user = Auth::login($data)) {
-                    Sesion::flash('Auth.succes', 'You are now logged as ' . Auth::user()->name);
-                    $this->redirect('/my-account');
-                } else {
-                    Session::flash('Auth.error', 'Username or Password invalid');
-                    $this->redirect('/login');
-                }
-            }
-        }
-    (end)
+	Example:
+		- Inside your UsersController for an example, you can create you login method like the code bellow
+		(start code)
+		public function login() {
+			if ($data = $this->data) {
+				if ($user = Auth::login($data)) {
+					Sesion::flash('Auth.succes', 'You are now logged as ' . Auth::user()->name);
+					$this->redirect('/my-account');
+				} else {
+					Session::flash('Auth.error', 'Username or Password invalid');
+					$this->redirect('/login');
+				}
+			}
+		}
+		(end)
 */
 class Auth {
     /*
-    Constant: SESSION_KEY
+        Constant: SESSION_KEY
 
-    Name of the key to be used to store the user's information in
-    the session.
+        Name of the key to be used to store the user's information in
+        the session.
     */
     const SESSION_KEY = 'Auth.user';
 
     /*
-    Variable: $userModel
+        Variable: $userModel
 
-    Name of the model used to identify users.
+        Name of the model used to identify users.
     */
     public static $userModel = 'Users';
 
     /*
-    Variable: $hash
+        Variable: $hash
 
-    Class and method to be used as a hash in authentication.
+        Class and method to be used as a hash in authentication.
     */
     public static $hash = array('Security', 'hash');
 
     /*
-    Variable: $fields
+        Variable: $fields
 
-    Names of the columns used by the model to identify the user.
-    Defaults are: 'username' for the identifier used by the user
-    and 'password' for the user's password.
+        Names of the columns used by the model to identify the user.
+        Defaults are: 'username' for the identifier used by the user
+        and 'password' for the user's password.
 
-    See Also:
-    <Auth::login>
+        See Also:
+            <Auth::login>
     */
     public static $fields = array(
         'username' => 'username',
         'password' => 'password'
-        );
-
+    );
 
     /*
-    Method: login
+        Method: login
 
-    Logs a user in using the data provided by the method's parameter.
+        Logs a user in using the data provided by the method's parameter.
 
-    Parameters:
-    $data - array containing at least the user's identifier and
-    its password. The array's keys are the same defined by
-    <Auth::$fields>.
+        Parameters:
+            $data - array containing at least the user's identifier and
+            its password. The array's keys are the same defined by
+            <Auth::$fields>.
 
-    Returns:
-    True if the user was logged in. False instead.
+        Returns:
+            True if the user was logged in. False instead.
 
-    See Also:
-    <Auth::$fields>
+        See Also:
+            <Auth::$fields>
     */
     public static function login($data) {
         $user = self::identify($data);
@@ -93,40 +93,40 @@ class Auth {
     }
 
     /*
-    Method: logout
+        Method: logout
 
-    Logs a user out by destroying the session.
+        Logs a user out by destroying the session.
     */
     public static function logout() {
         Session::delete(self::SESSION_KEY);
     }
 
     /*
-    Method: loggedIn
+        Method: loggedIn
 
-    Verifies if the current user is logged in.
+        Verifies if the current user is logged in.
 
-    Returns:
-    True if the user is logged in. False instead.
+        Returns:
+            True if the user is logged in. False instead.
     */
     public static function loggedIn() {
         return !is_null(Session::read(self::SESSION_KEY));
     }
 
     /*
-    Method: user
+        Method: user
 
-    Returns the current logged in user. Be aware that the user info
-    may not be accurate, because it reflects the data stored in the
-    session, not the database. If you want to update the user's info,
-    use <Auth::update>.
+        Returns the current logged in user. Be aware that the user info
+        may not be accurate, because it reflects the data stored in the
+        session, not the database. If you want to update the user's info,
+        use <Auth::update>.
 
-    Returns:
-    The current logged in user object, null if there is no user
-    logged in.
+        Returns:
+            The current logged in user object, null if there is no user
+            logged in.
 
-    See Also:
-    <Auth::update>
+        See Also:
+            <Auth::update>
     */
     public static function user() {
         if($this->loggedIn()) {
@@ -136,15 +136,15 @@ class Auth {
     }
 
     /*
-    Method: update
+        Method: update
 
-    Updates the user's object stored in the session.
+        Updates the user's object stored in the session.
 
-    Params:
-    $user - user's object.
+        Params:
+            $user - user's object.
 
-    See Also:
-    <Auth::user>
+        See Also:
+            <Auth::user>
     */
     public static function update($user) {
         if(self::loggedIn()) {
@@ -153,38 +153,38 @@ class Auth {
     }
 
     /*
-    Method: identify
+        Method: identify
 
-    Query the model to get a user identified by a username/password
-    pair. The names of the fields are defined in <Auth::$fields>.
+        Query the model to get a user identified by a username/password
+        pair. The names of the fields are defined in <Auth::$fields>.
 
-    Params:
-    $data - user data. The array keys used are defined in
-    <Auth::$fields>.
+        Params:
+            $data - user data. The array keys used are defined in
+            <Auth::$fields>.
 
-    Returns:
-    The user's object if one is found, null otherwise.
+        Returns:
+            The user's object if one is found, null otherwise.
 
-    See Also:
-    <Auth::$fields>
+        See Also:
+            <Auth::$fields>
     */
     protected static function identify($data) {
         extract(self::$fields);
         return Model::load(self::$userModel)->first(array(
-        'conditions' => array(
-            $username => $data[$username],
-            $password => call_user_func(self::$hash, array($data[$password]))
+            'conditions' => array(
+                $username => $data[$username],
+                $password => call_user_func(self::$hash, array($data[$password]))
             )
-            ));
+        ));
     }
 
     /*
-    Method: writeSession
+        Method: writeSession
 
-    Writes the serialized user's object to the session.
+        Writes the serialized user's object to the session.
 
-    Params:
-    $user - user's object.
+        Params:
+            $user - user's object.
     */
     protected static function writeSession($user) {
         Session::regenerate();
